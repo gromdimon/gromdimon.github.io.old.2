09f7fa041b7bf4821d5f1cd0fa681c4cf181cba7
Dzmitry Hramyka <gromdimon@Dzmitrys-MacBook-Air.local>
2024-03-29 16:50:15 +0100

    disable cv download

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -33,7 +33,7 @@ export default function Nav({ posts }) {
           </a>
 
           {/* Button for CV download */}
-          <a
+          {/* <a
             href={info.cv}
             download
             className="px-4 py-2 border-2 rounded text-secondary dark:text-dk-secondary border-secondary dark:border-dk-secondary hover:bg-secondary dark:hover:bg-dk-secondary hover:text-primary dark:hover:text-primary cursor-pointer absolute left-1/2 transform -translate-x-1/2"
@@ -43,7 +43,7 @@ export default function Nav({ posts }) {
               Download CV
             </span>
             <span className="lg:hidden font-medium">CV</span>
-          </a>
+          </a> */}
 
           <div className="inline-flex lg:hidden text-secondary dark:text-dk-secondary">
             <Hamburger
